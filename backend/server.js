24d63ad8d1a1dd2c85755e0c99aa6ab36b27ba94
Alok8777 <alokprasad85077@gmail.com>
2021-01-19 14:32:21 +0530

    port number changes

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,6 @@ connectDB();
 const app = express();
 app.use(express.json());
 app.use('/api/products', productRoutes);
-const PORT = process.env.PORT ;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(` server is running at port ${PORT}`));
