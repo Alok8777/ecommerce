73c9e3139e4fe98315d584abc24a6e82f8a49212
Alok8777 <alokprasad85077@gmail.com>
2021-01-19 14:37:49 +0530

    production code added

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const path = require('path');
 const express = require('express');
 const connectDB = require('./config/db');
 const productRoutes = require('./routes/productRoutes');
@@ -8,6 +9,15 @@ connectDB();
 const app = express();
 app.use(express.json());
 app.use('/api/products', productRoutes);
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
+    })
+};
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(` server is running at port ${PORT}`));
