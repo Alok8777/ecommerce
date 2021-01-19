1e8a844789a177273cf4418611beed6451ecb73d
Alok8777 <alokprasad85077@gmail.com>
2021-01-19 23:43:38 +0530

    third change

--- backend/data/products.js
+++ backend/data/products.js
@@ -1,56 +1,83 @@
 const products = [
     {
-      name: "PlayStation 5",
+      name: "English navi",
       imageUrl:
-        "https://images.unsplash.com/photo-1606813907291-d86efa9b94db?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80",
+        "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/productimage/2020/2/14/bc9a3560-c2fe-43db-b73f-bbd812c306271581635929942-2.jpg",
       description:
-        "PlayStation 5 (PS5) is a home video game console developed by Sony Interactive Entertainment. Announced in 2019 as the successor to the PlayStation 4, the PS5 was released on November 12, 2020 in Australia, Japan, New Zealand, North America, Singapore, and South Korea, and November 19, 2020 onwards in other major markets except China and India.",
+        "Black solid formal shirt, has a spread collar, long sleeves, button placket, and straight hem",
       price: 499,
       countInStock: 15,
     },
     {
-      name: "Iphone 12",
+      name: "JAINISH",
       imageUrl:
-        "https://images.unsplash.com/photo-1605787020600-b9ebd5df1d07?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1463&q=80",
+        "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/productimage/2020/1/17/d71045b2-4300-4395-b110-f652fa00e9e51579211471795-1.jpg",
       description:
-        "Welcome to a new era of iPhone. Beautifully bright 6.1-inch Super Retina XDR display.1 Ceramic Shield with 4x better drop performance.2 Incredible low-light photography with Night mode on all cameras. Cinema-grade Dolby Vision video recording, editing, and playback. Powerful A14 Bionic chip. And new MagSafe accessories for easy attach and faster wireless charging.3 Let the fun begin.",
-      price: 1099,
+        "Grey solid formal shirt, has a button-down collar, long sleeves, button placket, straight hem, and 1 patch pocket",
+      price: 399,
       countInStock: 10,
     },
     {
-      name: "Cannon EOS-1D",
+      name: "Shaftesbury London",
       imageUrl:
-        "https://images.unsplash.com/photo-1519183071298-a2962feb14f4?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
+        "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/998931/2015/9/24/11443094063436-Shaftesbury-London-Men-Shirts-9251443094063091-1.jpg",
       description:
-        "The EOS-1D X combines speed with image quality, to create the next generation camera for professionals. Full frame 18 megapixel sensor with Dual “DIGIC 5+” processors sets the standard, and up to 12 frames per second shooting takes it beyond.",
-      price: 1300,
+        "White formal shirt, has a spread collar, long sleeves, a full button placket, a patch pocket",
+      price: 300,
       countInStock: 5,
     },
     {
-      name: "Amazon Alexa",
+      name: "Hancock",
       imageUrl:
-        "https://images.unsplash.com/photo-1518444065439-e933c06ce9cd?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1267&q=80",
+        "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/5394086/2018/4/20/11524224859040-Hancock-Men-Black-Slim-Fit-Solid-Formal-Shirt-3071524224858865-1.jpg",
       description:
-        "It is capable of voice interaction, music playback, making to-do lists, setting alarms, streaming podcasts, playing audiobooks, and providing weather, traffic, sports, and other real-time information, such as news. Alexa can also control several smart devices using itself as a home automation system.",
-      price: 50,
+        "Black solid formal shirt, has a spread collar, button placket, 1 pocket, long sleeves, curved hem",
+      price: 350,
       countInStock: 25,
     },
     {
-      name: "Audio Technica Headphones",
+      name: "DENNISON",
       imageUrl:
-        "https://images.unsplash.com/photo-1558756520-22cfe5d382ca?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
+        "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/11166464/2019/12/23/2a8849a0-713b-4f90-ae30-2ecbb983005b1577105787982-DENNISON-Men-Purple-Comfort-Slim-Fit-Solid-Formal-Shirt-2301-1.jpg",
       description:
-        "Outfitted with 45mm large-aperture dynamic drivers and an over-ear, closed-back design, the ATH-M50x headphones deliver clarity, deep bass, and extended bandwidth (15 Hz to 28 kHz) while isolating you from outside sounds.",
-      price: 233,
+        "Wine-coloured solid formal shirt, has a spread collar, long sleeves, button placket, curved hem, and 1 patch pocket",
+      price: 599,
       countInStock: 4,
     },
     {
-      name: "JBL FLIP 4",
+      name: "JAINISH",
       imageUrl:
-        "https://images.unsplash.com/photo-1564424224827-cd24b8915874?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1489&q=80",
+        "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/productimage/2020/2/27/f06c3c6c-311e-4d8a-9551-13edab7863311582758864952-1.jpg",
       description:
-        "JBL Flip 4 is the next generation in the award-winning Flip series; it is a portable Bluetooth speaker that delivers surprisingly powerful stereo sound. This compact speaker is powered by a 3000mAh rechargeable Li-ion battery that offers up to 12 hours of continuous, high-quality audio playtime.",
-      price: 140,
+        "Navy Blue solid formal shirt, has a slim collar, button placket, 1 pocket, long sleeves, curved hem",
+      price: 749,
+      countInStock: 10,
+    },
+    {
+      name: "Sztori",
+      imageUrl:
+        "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/12190754/2020/12/10/025ca78b-35ad-4816-89d4-43c352dec1861607591239086-Sztori-Men-Plus-Size-White--Navy-Blue-Regular-Fit-Disty-Prin-1.jpg",
+      description:
+        "White and navy blue ditsy printed formal shirt, has a spread collar, long sleeves, button placket, and curvedComes with a matching mask",
+      price: 719, 
+      countInStock: 12,
+    },
+    {
+      name: "Louis Philippe",
+      imageUrl:
+        "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/13305584/2021/1/6/cf650a7d-14dc-4113-ab9a-37ab4b88fc4d1609935592780LouisPhilippeMenBurgundyBlueRegularFitCheckedFormalShirt1.jpg",
+      description:
+        "Burgundy and Blue checked formal shirt, has a spread collar, long sleeves, button placket, curved hem, and 1 patch pocket",
+      price: 1749,
+      countInStock: 19,
+    },
+    {
+      name: "RAMRAJ COTTON",
+      imageUrl:
+        "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/productimage/2020/12/29/b473938c-6486-497b-aa39-7ff6d5284a9e1609232599608-1.jpg",
+      description:
+        "Peach-Coloured solid formal shirt, has a spread collar, short sleeves, button placket, straight hem, and 1 patch pocket",
+      price: 749,
       countInStock: 10,
     },
   ];
